5415143287e2638a48c3bb1103c7016ce27337b2
kogi <101094265+KogiWaweru@users.noreply.github.com>
2022-05-15 18:53:42 +0300

    Add files via upload

---
+++ anusimh/index.js
@@ -0,0 +1,12 @@
+
+// Show and hide faq
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+        // change icon
+        // const icon = faq.querySelector('.faq__icon')
+        // if(icon.className == )
+    })
+})
